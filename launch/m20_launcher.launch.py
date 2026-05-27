6d291627e4b44df0a18937266885a17bf5b2b594
o-tatchan <t-okamoto@hokuyo-aut.co.jp>
2026-05-27 02:50:28 +0000

    05/27 update launch file

--- launch/m20_launcher.launch.py
+++ launch/m20_launcher.launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -14,7 +15,8 @@ def generate_launch_description():
     # launch argument
     ld = LaunchDescription([
         DeclareLaunchArgument('sensor_ip_', default_value="192.168.0.10", description='IP of Lumotive sensor to connect to.'),
-        DeclareLaunchArgument('sensor_port_', default_value="10940", description='Port of Lumotive sensor to connect to'),
+        DeclareLaunchArgument('sensor_port_', default_value="10940", description='Port of Lumotive sensor to connect to.'),
+        DeclareLaunchArgument('launch_api_client_', default_value="true", description='Launch flag of api client. if true, YLM auto start scan.')
     ])
     
     # load yaml file
@@ -23,7 +25,6 @@ def generate_launch_description():
         'config',
         'm20_configs.yaml'
     )
-
 
     yaml_configs = yaml.safe_load(open(config_path, 'r'))
     configs = yaml_configs['lumotive_ros_params']
@@ -36,6 +37,15 @@ def generate_launch_description():
         output='screen',
         parameters = [configs]
         )
-
     ld.add_action(drv_node)
+    
+    api_node = Node(
+        package='ylm_ros2',
+        executable='api_driver',
+        output='screen',
+        parameters = [configs],
+        condition = IfCondition(LaunchConfiguration('launch_api_client_'))
+        )
+    ld.add_action(api_node)
+    
     return ld
